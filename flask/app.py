fb2a79e448a9f88eaf984141a0891d270029bc90
jinju <jeongjinju7031@gmail.com>
2019-08-01 18:00:35 +0900

    0801

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, escape, request, render_template
 import random
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -53,6 +54,51 @@ def search():
     user_tier = tier.text.strip()
     # print(tier.text.strip())
     return render_template("search.html", user_tier=user_tier, summoner=summoner)
+
+@app.route("/nono")
+def nono():
+
+    with open("data.csv", "r", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        products = list(reader)
+
+    return render_template("nono.html", products=products)
+    # nono.html은 안에 html 언어가 아닌 다른 언어도 씌여있는 불완전한 html
+    # 크롬이 인식할 수 있도록 완전한 html 로 바꿔주는 것이 render_template
+
+@app.route("/new")
+# 데이터 입력할 수 있는 페이지
+def new():
+    return render_template("new.html")
+
+@app.route("/create")
+def create():
+    product = request.args.get('product')
+    category = request.args.get('category')
+    replace = request.args.get('replace')
+   
+    
+    with open("data.csv", "a+", encoding="utf-8", newline="") as f:
+    # a+ 는 사용자가 계속 데이터를 입력할 것이기 떄문에 한줄 한줄 계속 추가하라는 것.
+        writer = csv.writer(f)
+        #["해피해킹","키보드","대체상품"]
+        product_info = [product, category, replace]
+        writer.writerow(product_info)
+        # list를 넘겨주면 하나의 row로 판단하여 값을 입력
+        # crud logic
+        # create , read, update, delete
+        # 
+
+    return render_template("create.html")
+    # html 이 없다면 template not found - 404 not found
+
+@app.route("/card")
+def card():
+    with open("data.csv", "r", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        products = list(reader)
+
+    return render_template("card.html", products=products)
 
 if __name__ == "__main__":
     app.run(debug=True)
